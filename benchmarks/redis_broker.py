09139162e466107fc7dcf624f15ec8a443c2b97a
komuw <komuw05@gmail.com>
2019-04-21 15:35:38 +0300

    f

--- benchmarks/redis_broker.py
+++ benchmarks/redis_broker.py
@@ -13,6 +13,7 @@ class ExampleRedisBroker(wiji.broker.BaseBroker):
     DO NOT USE THIS IN PRODUCTION.
     It will fail you, this is only used for adhoc testing
     and for showing how easy it is to implement your own broker satisfying `wiji.broker.BaseBroker`
+
     use redis as our queue.
     This implements a basic FIFO queue using redis.
     Basically we use the redis command LPUSH to push messages onto the queue and BRPOP to pull them off.
