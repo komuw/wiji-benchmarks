560c804be1a3d60d0eef773028ff66f76c86f435
komuW <komuw05@gmail.com>
2019-05-06 17:50:30 +0300

    f

--- cool.py
+++ cool.py
@@ -1,21 +1,14 @@
 import json
 
 
-result = """
+result_head = """
 Queuing metrics results:     
 | Task name      |  Numober of tasks queued | Time to queue 1 task(sec) | Number of tasks dequeued | Time to execute 1 task(sec) |
-| :---           |  ---:                    |  ---:                     |   ---:                   |  ---:                       |       
-| {task_name}    |  {tasks_queued}          |  {queuing_duration}       |  {tasks_dequeued}        |  {execution_duration}       |
+| :---           |  ---:                    |  ---:                     |   ---:                   |  ---:                       |    
 """
 
-#   "task_name-DiskIOTask": {
-#     "tasks_queued": 4243,
-#     "queuing_duration": 0.05,
-#     "tasks_dequeued": 4243,
-#     "execution_duration": 0.01
-#   }
-# with open("./tmp/metrics/ala.md", mode="w") as f:
-#     f.write(x.format(sec=90))
+result_body = """| {task_name}    |  {tasks_queued}          |  {queuing_duration}       |  {tasks_dequeued}        |  {execution_duration}       |
+"""
 
 
 def cool():
@@ -27,22 +20,19 @@ def cool():
 
 final_queuing_metrics = cool()
 
-for i in final_queuing_metrics.keys():
-    print(i)
 
-    _result = result.format(
+all_res = []
+for i in final_queuing_metrics.keys():
+    _result = result_body.format(
         task_name=i,
         tasks_queued=final_queuing_metrics[i].get("tasks_queued"),
         queuing_duration=final_queuing_metrics[i].get("queuing_duration"),
         tasks_dequeued=final_queuing_metrics[i].get("tasks_dequeued"),
         execution_duration=final_queuing_metrics[i].get("execution_duration"),
     )
-import pdb
+    all_res.append(_result)
 
-pdb.set_trace()
-print()
-print()
-print("l")
 
+final_markdwon = result_head + "".join(all_res)
 with open("./tmp/metrics/ala.md", mode="w") as f:
-    f.write(_result)
+    f.write(final_markdwon)
