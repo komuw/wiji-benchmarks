f9d8c5ef1457d785103b1d1e03172aa49413d74d
komuW <komuw05@gmail.com>
2019-05-20 21:23:01 +0300

    f

--- digitalocean.sh
+++ digitalocean.sh
@@ -8,18 +8,25 @@ else
 fi
 shopt -s nullglob globstar
 
-export DEBIAN_FRONTEND=noninteractive
-
+export DEBIAN_FRONTEND=noninteractive && \
 apt -y update && \
 apt -y install python && \
-apt -y install python-pip nano && \
-snap install docker && \
-apt -y install wget unzip && \
+apt -y install python-pip nano wget unzip curl
+
+# NB: do not install docker from snap; it is broken
+apt -y remove docker docker-engine docker.io containerd runc && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+apt-key fingerprint 0EBFCD88 && \
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
+apt -y update; apt -y autoremove && \
+apt -y install docker-ce && \
+usermod -aG docker $(whoami) && \
+pip install -U docker-compose
+
 wget https://github.com/komuw/wiji-benchmarks/archive/master.zip && \
 unzip master.zip && \
 mv wiji-benchmarks-master/ wiji-benchmarks && \
 cd wiji-benchmarks/
-
 
 # edit `compose.env` to add neccesary credentials
 screen -S wiji
